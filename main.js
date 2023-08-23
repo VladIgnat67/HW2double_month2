501edcfb8dd16e381247297a510f9f32b1b59fbb
VladIgnat67 <vldslvbolotov@gmail.com>
2023-08-23 15:57:01 +0600

    HW2-Double

---
+++ main.js
@@ -0,0 +1,52 @@
+// 1 задание "Треугольник в цилке"
+ 
+// var p90 = "#";
+// while (p90.length<=7){
+// 	console.log(p90);
+// 	p90+="#";
+// }
+
+
+// 2 Задание "FizzBuzz"
+// for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(null);
+// }
+
+// 3 задание "Шахматная доска"
+// function createChess(a, b) {
+//     var value1 = "# ";
+//     var value2 = " #";
+//     var odd = "";
+//     var even = "";
+//     for (var i = 0; i < a / 2; i += 1) {
+//       odd += value2;
+//       even += value1;
+//     }
+  
+//     for (var j = 0; j < b; j += 1) {
+//       if (j % 2 != 0) {
+//         console.log(odd);
+//       } else {
+//         console.log(even);
+//       }
+//     }
+//   }
+  
+//   createChess(16, 8);
+
+
+// Доп 1 задание 
+// const numbers = ['Алекс', 'Анна', 'Олег'];
+
+// numbers.push(1, 2, 3);
+// console.log(numbers);
+
+
+// Доп 2 задание
+// function factorial(n){
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+// }
+// console.log( factorial(7) )
